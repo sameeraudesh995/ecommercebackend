eb4d0aaf3e4f6c86932511bb2df9172e4c26f879
Sameera Udesh <sameeraudesh75@gmail.com>
2026-04-12 23:26:18 +0530

    add 2

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const { error } = require("console");
 
 app.use(express.json());
 app.use(cors());
@@ -123,6 +124,81 @@ app.get('/allproducts',async(req,res)=>{
     let products = await Product.find({})
     console.log("All Product Fetched")
     res.send(products);
+})
+
+//Schema create for user model
+const Users = mongoose.model('Users',{
+    name:{
+        type:String,
+
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+})
+
+// creating end point fir registering the user
+app.post('/signup',async(req,res)=>{
+    let check = await Users.findOne({email:req.body.email});
+    if(check){
+        return res.status(400).json({success:false,errors:"existing user found with same email address"})
+    }
+    let cart ={}
+    for (let i =0; i < 300; i++) {
+        cart[i] =0;
+        
+    }
+    const user = Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+
+    const data ={
+        user:{
+            id:user.id
+        }
+    }
+
+    const token = jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
+})
+
+//creating endpoint for user login
+app.post('/login',async (req,res)=>{
+    let user = await Users.findOne({email:req.body.email});
+    if(user){
+        const passCompare =req.body.password === user.password;
+        if(passCompare){
+            const data = {
+                user:{
+                    id:user.id
+                }
+            }
+            const token = jwt.sign(data,'secret_ecom');
+            res.json({success:true,token})
+        }
+        else{
+            res.json({success:false, error:"Wrong Password"})
+        }
+    }
+    else{
+        res.json({success:false, error:"Wrong Email id"})
+    }
 })
 
 // Start server
